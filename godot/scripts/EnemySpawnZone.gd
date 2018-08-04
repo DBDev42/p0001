a576667c611b8012b2f183bc9be0fd5a630b5af9
DBDev42 <dbdev42@gmail.com>
2018-08-04 02:28:58 +0100

    Factory pattern added as singleton StateMachine added to control story Controller patter added to level Collision between shoots and enemies working

--- godot/scripts/EnemySpawnZone.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var screen_size = get_viewport().size
-
-func _ready():
-	pass
-
-func get_spawn_position():
-	return Vector2(screen_size.x * randf(), self.get_position().y)
